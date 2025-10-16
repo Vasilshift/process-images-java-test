49b4c1e58442cf9e19d307f9e4873d74ab8d55c4
evgeniivasilenko <evvbelgrad@gmail.com>
2025-10-15 22:02:21 -0400

    processing

--- src/main/java/com/tenderizz/test_endpoint/SecurityConfig.java
+++ src/main/java/com/tenderizz/test_endpoint/SecurityConfig.java
@@ -1,21 +1,21 @@
-//package com.tenderizz.test_endpoint;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authz -> authz
-//                        .requestMatchers("/actuator/**", "/api/v1/ping").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .csrf().disable();
-//        return http.build();
-//    }
-//}
+package com.tenderizz.test_endpoint;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/actuator/**", "/api/v1/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf().disable();
+        return http.build();
+    }
+}
